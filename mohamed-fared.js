a02f6ad296075a4a16047d009454a7437d52213b
weeehabla <weeehaplay@gmail.com>
2019-11-07 11:56:14 +0100

    my answers

---
+++ mohamed-fared.js
@@ -0,0 +1,17 @@
+
+ 
+
+ $("#Create").on("click",function(){
+
+ 	var check1 = $("#input-1")
+ 	var check2 = $("#input-2")
+ 	var check3 = $("#input-3")
+
+
+ 	if (check1.prop("checked")){
+
+ 		console.log("Fdf")
+ 	}
+
+ })
+
--- summary-assessment.js
+++ summary-assessment.js
@@ -49,8 +49,7 @@ function each(coll, f) {
     });
     return acc;
   }
-  
-  //=============================================================================
+   //=============================================================================
   /*                              Q1                                           */
   //=============================================================================
   //write a function that takes a string as an input and returns an array
@@ -58,9 +57,19 @@ function each(coll, f) {
   //solve it using the most appropriate helper functions(reduce,each,map,filter).
   //wordLengths("hello its me") // [5,3,2]
   
+  // used split to create array from the sring so i can litrate throgh and  map to generate new array 
   function wordLengths(str) {
-      // TODO: your code here 
-  }
+    
+    var array = str.split(" "); 
+
+    return map (array ,function(element) {
+
+        return element.length ;
+      })  
+     
+    }
+         // TODO: your code here 
+  
   
   //=============================================================================
   /*                                  Q2                                    */
@@ -70,8 +79,17 @@ function each(coll, f) {
   //solve it using the most appropriate helper functions(reduce,each,map,filter).
   // countOccurrences("hello", "l"); // 2
   // countOccurrences("hello, world!", "l"); // 3
+
+  // I used each to litaret through the the string and in every time the condition is right will add one to count 
   
   function countOccurrences(string, character) {
+    var count = 0 ; 
+    each(string, function(element) {
+      if (element ===  character) {
+        count ++;
+      }
+    }) 
+    return count ;
       // your code is here
   }
   
@@ -82,8 +100,18 @@ function each(coll, f) {
   //with only the words of length that are longer than 3.
   //solve it using the most appropriate helper functions(reduce,each,map,filter).
   // wordsLongerThanThree("Hello Mad World") //["Hello", "World"]
+
+  // i splited the sting then used the filter function to meet the right condition so in the end will return a new array with the result 
   
   function wordsLongerThanThree(str) {
+
+    var array = str.split(' ');
+
+     return filter(array , function(element){
+
+      return element.length > 3;
+
+    })
       // TODO: your code here 
   }
   
@@ -97,9 +125,23 @@ function each(coll, f) {
   //repeatString('dog', 1); // => 'dog' 
   //repeatString('dog', 2); // => 'dog' + 'dog' => 'dogdog' 
   //repeatString('dog', 3); // => 'dog' + 'dog' + 'dog' => 'dogdogdog'
+
+  // its a recursion :)) im still wondering how i solve it :)) 
   
   function repeatString(str, count) { 
-   // TODO: your code here 
+    
+    if (count === 0 ) {
+
+
+      return ""
+    }
+
+    else if (count > 0) {
+
+      // here is where the call of the funcation agin happen and reduce the count by one every time 
+      return str + repeatString(str, count-1) 
+    }
+
   } 
    
   
@@ -127,6 +169,44 @@ function each(coll, f) {
   // pizza.eatSlice();
   // pizza.eatSlice();
   // pizza.eatSlice();
+
+  function makePizza(crust, size, numberOfSlice) {
+    var curst = crust
+    var size = size
+    var ingredients =""
+    var numberOfSlice = numberOfSlice
+
+     //var ingredients  = "curst :"+crust+","+"size :"+size+","+"number of Slice :"+numberOfSlice
+     return {
+      addIngredients : function(str){
+
+        ingredients = ingredients +str+","
+
+      },
+      displayIngredaints: function(){
+
+         return " The ingredients are : "+ingredients
+      },
+      bakePizza : function(){
+
+         setTimeout(function(){ console.log( "Your "+crust+" "+size+" "+numberOfSlice+" slice pizza is done")},2000)
+
+      },
+      eatSlice : function(){
+
+        if(numberOfSlice > 0 ){
+
+          numberOfSlice--
+        }
+        else{
+
+          return "Sorry no more pizza for you"
+        }
+
+      }
+
+     }
+  }
   
   // Write your code here .....
   
@@ -153,7 +233,51 @@ function each(coll, f) {
   */
   
   // Now, to make sure that you are actually reading, make a comment below this and type: Yes I am
+  // Yes  I am 
+
+  function ReadingList() {
+
+    var readingList = {};
+    readingList.read = 0;
+    readingList.unRead = 0;
+    readingList.toRead = [];
+    readingList.currentRead = undefined;
+    readingList.readBooks = [];
+    readingList.addBook = addBook;
+    readingList.finishCurrentBook = finishCurrentBook
+
+    return readingList ;
+
+  }
+
+  // this function will add the book to the array
+
+  var addBook = function(name){
+
+    this.toRead.push(name);
+    this.unRead++;
+    
+  }
+
+  // i stucked here in this function problem with the courrentrRead in first time you add a book to the readingList.toRead 
+
+  var finishCurrentBook = function(){
+
+    // if(this.currentRead === "") {
+
+    //  this.currentRead = this.toRead[0];
+    // }
+    if (this.currentRead !== undefined)
+
+      this.read++
+      this.unRead--
+      this.readBooks.push(this.currentRead)
+      this.currentRead = this.toRead[0];
+
+  }
+
   
+
   // Write your code here .....
   
   //=============================================================================
@@ -173,6 +297,48 @@ function each(coll, f) {
   //  safe('gold-bar','big')
   //  safe('silver-bar','big') => "Can't fit"
   //  safe('money','small') => "watch gold-bar money"
+
+
+
+  function makeSafe(init) {
+
+    var init = init 
+    var str = '';
+
+    // return the function with 
+
+    return function(item, itemSize){
+
+      //coverting the string names to numbers 
+
+      if (itemSize === "small" ) {
+        itemSize = 1;
+      }
+      else if (itemSize === "medium" ) {
+        itemSize = 2;
+      }
+      else if ( itemSize === "big" ) {
+        itemSize = 3;
+      }
+
+      if (itemSize <=init) {
+
+        str = str + item ;
+        init = init - itemSize  
+        
+      }
+      else if ( init < 0 ) {
+
+        return str ; 
+      }
+
+      else if ( itemSize > init) {
+
+        return "Can't fit"
+      }
+        
+    }
+  }
   
   // Write your code here .....
   
@@ -216,10 +382,19 @@ function each(coll, f) {
   //================================================================================
   // Theoretical questions.
   // 1- In your own words,Why do we use Closures ?
+  // we use closures when need to declare a globel variable outside the funtion  
   
   // 2- In OOP, what does "this" refer to ?
+  // when we need to grap the globel varabile in class we use this in the outter function 
+  // so we can access the opject properties 
   
   // 3- What is jQuery?
+  // it a javascript libary that simplfy the using of document object model 
   
   // 4- what is the diffrence between Closure's methods and The OOP's methods?
+  // the the passing arrgemnt in the clousers will be a globle scope to inner funcation 
+  //and it will use it , in oop all the method functions will be refered out side the class 
+  // so we dont use more sapce for each instence that we create by the class . 
   
+
+
